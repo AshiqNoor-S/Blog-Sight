c4958f50fd9f602ea941491ccee0fe97f9f0ea2c
AshiqNoor-S <ashiqnoorsudheer@gmail.com>
2023-03-30 00:38:00 +0530

    Created the React App

--- blogsight/src/index.js
+++ blogsight/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./Components/App";
 
-ReactDOM.render(<h1>Hello</h1>,document.getElementById('root'));
 
-
+ReactDom.render(<App />, document.getElementById("root"));
