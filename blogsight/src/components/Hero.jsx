293e1d8ab06c700cf6e9ed51bc320b46c21d3051
Priyanshu Pattanaik <hi_priyanshu@outlook.com>
2023-03-31 01:33:32 +0530

    Add hover effects, update trending section button

--- blogsight/src/components/Hero.jsx
+++ blogsight/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Hero(){
     return (
         <div className="hero">
-        <h1>BlogSight</h1>
+        <h1 className="page-title">BlogSight</h1>
         <img class="girl" src="../images/girl.png" alt="" />
         <img class="gadget" src="../images/gadgets.png" alt="" />
         <img class="beat" src="../images/beat.png" alt="" />
--- blogsight/src/components/MostTrendingSection.jsx
+++ blogsight/src/components/MostTrendingSection.jsx
@@ -4,9 +4,9 @@ import TrendingPost from "./TrendingPost";
 function MostTrendingSection(){
 	return (
 		<div className="most-trending-section">
-            <h2 className="most-trending-heading">Most Trending</h2>
+            <h1 className="most-trending-heading">MOST TRENDING</h1>
             <TrendingPost title="Sample Trending Topic" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quasi iusto id dolores laboriosam accusamus commodi repellat maxime! Soluta eveniet neque enim, perspiciatis officia aliquam reiciendis rerum repellendus numquam itaque, debitis exercitationem doloribus necessitatibus, officiis vel ex animi. Rerum non sunt porro saepe vitae obcaecati illo consequuntur dignissimos quisquam et excepturi, amet cupiditate commodi quasi aspernatur culpa deserunt perspiciatis quidem fugiat enim minus. Sint, ut ratione. Accusantium, aliquam. Nobis corporis dolorem sed magni eius consequatur nihil animi, deserunt molestiae, explicabo reiciendis ipsa soluta voluptatem dolorum! Omnis voluptates nisi architecto illum provident quae recusandae. Ipsa ex asperiores facere dolorum autem iusto." />
-            <button>Start Reading</button>
+            <button className="start-reading-button">Start Reading</button>
         </div>
 	);
 }
