258ad509781bd7955be5934b265744bb5b0784c7
AshiqNoor-S <ashiqnoorsudheer@gmail.com>
2023-03-31 00:21:36 +0530

    Vectors added

--- blogsight/src/components/Hero.jsx
+++ blogsight/src/components/Hero.jsx
@@ -4,6 +4,9 @@ function Hero(){
     return (
         <div className="hero">
         <h1>BlogSight</h1>
+        <img class="girl" src="../images/girl.png" alt="" />
+        <img class="gadget" src="../images/gadgets.png" alt="" />
+        <img class="beat" src="../images/beat.png" alt="" />
         </div>
     );
 }
