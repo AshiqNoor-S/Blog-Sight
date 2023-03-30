4850160394bd26cd032e09557a62db931ef3099a
Priyanshu Pattanaik <hi_priyanshu@outlook.com>
2023-03-31 00:18:49 +0530

    Final styles for team section

--- blogsight/src/components/App.jsx
+++ blogsight/src/components/App.jsx
@@ -6,13 +6,17 @@ import members from "../team-members";
 function App(){
     return (<div>
         <Hero />
-        {members.map(member => (
-            <TeamMember 
-                key={member.id} 
-                name={member.name} 
-                imgURL={member.imgURL}
-            />
-        ))}
+        {/* The Team */}
+        <div className="the-team-section">
+            <h2 className="the-team-heading">THE TEAM</h2>
+            {members.map(member => (
+                <TeamMember 
+                    key={member.id} 
+                    name={member.name} 
+                    imgURL={member.imgURL}
+                />
+            ))}
+        </div>
     </div>
 );}
 
--- blogsight/src/components/TeamMember.jsx
+++ blogsight/src/components/TeamMember.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function TeamMember(props){
-	return (<div>
+	return (<div className="member">
 			<img src={props.imgURL} className="member-img" alt="Team member" />
 			<p className="member-name">{props.name}</p>
 	</div>);
