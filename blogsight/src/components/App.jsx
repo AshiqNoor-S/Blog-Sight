6d543531021d9a46a8c51869138209b52eba9543
AshiqNoor-S <ashiqnoorsudheer@gmail.com>
2023-03-31 00:46:28 +0530

    Added trending section

--- blogsight/src/components/App.jsx
+++ blogsight/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import Trending from "./Trending"
 import TeamSection from "./TeamSection";
 
 function App(){
     return (<div>
         <Hero />
+        <Trending />
         <TeamSection />
     </div>
 );}
