e4973fa411ec449bd01e62975a00f32cc8c4f302
Priyanshu Pattanaik <hi_priyanshu@outlook.com>
2023-03-31 11:23:56 +0530

    Add footer with styles

--- blogsight/src/components/App.jsx
+++ blogsight/src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./Hero";
 import TeamSection from "./TeamSection";
 import MostTrendingSection from "./MostTrendingSection";
+import Footer from "./Footer";
 
 function App(){
     return (<div>
         <Hero />
         <MostTrendingSection />
         <TeamSection />
+        <Footer />
     </div>
 );}
 
---
+++ blogsight/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Footer(){
+	return (
+		<div className="footer">
+			<div className="reach-out-column">
+				<h3 className="reach-out-text">Reach Out To Us</h3>
+				<div className="reach-out-icons-row">
+					<a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+					<a href="#">
+					<i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+				</div>
+			</div>
+			<div className="reach-out-centre-column">
+				<h1 className="company-name">BlogSight</h1>
+				<p className="copyright-text">Copyright &copy; BlogSight</p>
+			</div>
+			<div className="reach-out-contact-column">
+				<h3 className="contact-us-text">Contact Us</h3>
+				<div className="contact-us-icons-row">
+					<a href="#"><i class="fa fa-phone fa-2x" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
