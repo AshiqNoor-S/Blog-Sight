779cfd1580b83177f3b5e495494223b3b66bc5a6
AshiqNoor-S <ashiqnoorsudheer@gmail.com>
2023-06-01 13:09:04 +0530

    latest

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import BlogHome from "./BlogHome";
 import Blogs from "./Blogs";
 import NewBlogPost from "./NewBlogPost";
 import Footer from "./Footer";
+import Login from "./Login"
 import { Routes, Route } from "react-router-dom";
 import PatientMonitoring from "./PatientMonitoring";
 
@@ -14,7 +15,7 @@ function App(){
         {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/blog-home" element={<BlogHome />} /> */}
         <Route path="/" element={<BlogHome />} />
-        {/* <Route path="/Login" element={<Login />}></Route> */}
+        <Route path="/login" element={<Login />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/newblogpost" element={<NewBlogPost />} />
         {/* <Route path="/patient-monitoring" element={<PatientMonitoring />} /> */}
--- src/components/MostTrendingSection.jsx
+++ src/components/MostTrendingSection.jsx
@@ -6,7 +6,7 @@ function MostTrendingSection(){
 		<div className="most-trending-section">
             <h1 className="most-trending-heading">MOST TRENDING</h1>
             <TrendingPost title="Sample Trending Topic" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quasi iusto id dolores laboriosam accusamus commodi repellat maxime! Soluta eveniet neque enim, perspiciatis officia aliquam reiciendis rerum repellendus numquam itaque, debitis exercitationem doloribus necessitatibus, officiis vel ex animi. Rerum non sunt porro saepe vitae obcaecati illo consequuntur dignissimos quisquam et excepturi, amet cupiditate commodi quasi aspernatur culpa deserunt perspiciatis quidem fugiat enim minus. Sint, ut ratione. Accusantium, aliquam. Nobis corporis dolorem sed magni eius consequatur nihil animi, deserunt molestiae, explicabo reiciendis ipsa soluta voluptatem dolorum! Omnis voluptates nisi architecto illum provident quae recusandae. Ipsa ex asperiores facere dolorum autem iusto." />
-            <button className="start-reading-button"><a href="/blogs">Start Reading</a></button>
+            <button className="start-reading-button"><a href="/login">Start Reading</a></button>
         </div>
 	);
 }
