c1becfc6f923d0b94eb3a170d78ecbbe988a8d7c
AshiqNoor-S <ashiqnoorsudheer@gmail.com>
2023-06-01 13:39:33 +0530

    basic authentication functionality added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React , {useState} from "react";
 import Home from "./Home";
 import BlogHome from "./BlogHome";
 import Blogs from "./Blogs";
 import NewBlogPost from "./NewBlogPost";
 import Footer from "./Footer";
 import Login from "./Login"
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
 import { Routes, Route } from "react-router-dom";
 import PatientMonitoring from "./PatientMonitoring";
 
 function App(){
+    const [isAuth,setIsAuth] = useState(false);
     return (
     <div>
     <Routes>
         {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/blog-home" element={<BlogHome />} /> */}
         <Route path="/" element={<BlogHome />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/newblogpost" element={<NewBlogPost />} />
         {/* <Route path="/patient-monitoring" element={<PatientMonitoring />} /> */}
