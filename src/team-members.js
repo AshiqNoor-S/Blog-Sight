9d428aa80dfea96c7cef8ee2f81db0479dfbe13c
Priyanshu Pattanaik <hi_priyanshu@outlook.com>
2023-06-02 15:28:36 +0530

    Home Page: Team Members URL added

--- src/team-members.js
+++ src/team-members.js
@@ -2,23 +2,26 @@ const members = [
 	{
 		id: 1, 
 		name: "Ashiq Noor Sudheer",
-		imgURL: "../images/ashiq.jpg"
+		imgURL: "../images/ashiq.jpg",
+		socialsURL: "https://linktr.ee/ashiqnoorsudheer"
 	},
 	{
 		id: 2, 
 		name: "Priyanshu Pattanaik",
-		imgURL: "../images/priyanshu.jpeg"
+		imgURL: "../images/priyanshu.jpeg",
+		socialsURL: "https://linktr.ee/priyanshu_pattanaik"
 	},
 	{
 		id: 3, 
 		name: "Mathew Thomas",
-		imgURL: "../images/mathew.jpeg"
-		
+		imgURL: "../images/mathew.jpeg",
+		socialsURL: "https://linktr.ee/mathew_creates"
 	},
 	{
 		id: 4, 
 		name: "Ryan Ranjit Abraham",
-		imgURL: "../images/ryan.jpeg"
+		imgURL: "../images/ryan.jpeg",
+		socialsURL: "https://linktr.ee/RYAN_RANJIT_ABRAHAM"
 	}
 ]
 
